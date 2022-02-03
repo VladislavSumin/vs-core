eb77fb6198c6ea4613e5a3d71dd1e0bef555be58
vladislavsumin <vladislavsumin@mail.ru>
2022-02-03 18:15:29 +0300

    change convention location

---
+++ build-script/src/main/kotlin/ru/vs/convention/multiplatform/jvm.gradle.kts
@@ -0,0 +1,12 @@
+package ru.vs.convention.multiplatform
+
+import gradle.kotlin.dsl.accessors._ff1e04f882187441ad8eef90c6cec170.kotlin
+import org.gradle.kotlin.dsl.kotlin
+
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    jvm()
+}

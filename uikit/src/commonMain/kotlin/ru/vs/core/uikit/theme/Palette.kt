3ba617f0accead7eff82555705ee0358a8c9f955
vladislavsumin <vladislavsumin@mail.ru>
2022-02-07 19:09:26 +0300

    setup dark theme

--- uikit/src/commonMain/kotlin/ru/vs/core/uikit/theme/Palette.kt
+++ uikit/src/commonMain/kotlin/ru/vs/core/uikit/theme/Palette.kt
@@ -2,11 +2,18 @@ package ru.vs.core.uikit.theme
 
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
 
 internal val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+
+    background = Color(0xFF2B2B2B),
+    surface = Color(0xFF3B3F41),
+
+    onPrimary = Color(0xFFBBBBBB),
+    onSurface = Color(0xFFBBBBBB)
 )
 
 internal val LightColorPalette = lightColors(

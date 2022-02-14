a640ea5bd9f691137160b74aaffe3fad66ed112b
Vladislav Sumin <vladislavsumin@mail.ru>
2022-02-14 18:43:15 +0300

    make simple screens

--- core/navigation/src/commonMain/kotlin/ru/vs/core/navigation/ui/NavigationView.kt
+++ core/navigation/src/commonMain/kotlin/ru/vs/core/navigation/ui/NavigationView.kt
@@ -2,14 +2,16 @@ package ru.vs.core.navigation.ui
 
 import androidx.compose.runtime.Composable
 import com.arkivanov.decompose.extensions.compose.jetbrains.Children
+import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.ChildAnimation
 import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.crossfade
+import ru.vs.core.navigation.Screen
 
 @Composable
-fun NavigationContentView() {
+fun NavigationContentView(animation: ChildAnimation<Screen, ScreenInstance> = crossfade()) {
     val routerState = LocalNavigation.current.state
     Children(
         routerState = routerState,
-        animation = crossfade(),
+        animation = animation,
     ) { childCreated ->
         childCreated.instance.ScreenInstanceView()
     }

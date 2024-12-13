d7c6c0dd062ee67783a70deb63f20a990a56a797
Vladislav Sumin <vladislavsumin@mail.ru>
2024-12-14 00:06:08 +0300

    add kotlin multiplatform common convention

---
+++ build-scripts/src/main/kotlin/ru/vs/utils/VersionCatalogsUtils.kt
@@ -0,0 +1,10 @@
+package ru.vs.utils
+
+import org.gradle.accessors.dm.LibrariesForLibs
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.the
+
+/**
+ * Предоставляет доступ к каталогу версий внутри convention плагинов и прочего кода.
+ */
+val Project.libs: LibrariesForLibs get() = rootProject.the()

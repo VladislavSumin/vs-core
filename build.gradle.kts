8e823315d6fbc6f995d4dfa89532a8bd10f1ffea
Vladislav Sumin <vladislavsumin@mail.ru>
2025-03-27 22:23:17 +0300

    Fix java version to 17 && add strong jvm version check

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
+import ru.vladislavsumin.configuration.projectConfiguration
+
 plugins {
     id("ru.vladislavsumin.convention.analyze.detekt-all")
     id("ru.vladislavsumin.convention.analyze.check-updates")
+}
+
+val currentJavaVersion = JavaVersion.current().majorVersion
+require(currentJavaVersion == projectConfiguration.core.jvmVersion) {
+    "Project require java ${projectConfiguration.core.jvmVersion}, but current java version is $currentJavaVersion"
 }

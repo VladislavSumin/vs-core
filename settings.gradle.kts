d4003992a881a99751426918947970dd0efdf878
Vladislav Sumin <vladislavsumin@mail.ru>
2025-08-07 21:43:24 +0300

    add navigation di (#55)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,6 +27,7 @@ include(":core:logger:platform")
 
 include(":core:navigation:api")
 include(":core:navigation:debug")
+include(":core:navigation:di")
 include(":core:navigation:impl")
 include(":core:navigation:factory-generator:api")
 include(":core:navigation:factory-generator:ksp")

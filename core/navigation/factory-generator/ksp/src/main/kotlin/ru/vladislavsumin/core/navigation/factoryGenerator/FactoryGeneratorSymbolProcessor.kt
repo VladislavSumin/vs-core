a1555f765aa1e934d24a127700f50dc2be08a44d
Vladislav Sumin <vladislavsumin@mail.ru>
2025-08-12 21:59:42 +0300

    fix detekt

--- core/navigation/factory-generator/ksp/src/main/kotlin/ru/vladislavsumin/core/navigation/factoryGenerator/FactoryGeneratorSymbolProcessor.kt
+++ core/navigation/factory-generator/ksp/src/main/kotlin/ru/vladislavsumin/core/navigation/factoryGenerator/FactoryGeneratorSymbolProcessor.kt
@@ -90,7 +90,7 @@ internal class FactoryGeneratorSymbolProcessor(
         // 1) Найти параметр конструктора наследующийся от ScreenParams, это и будут наши искомые параметры.
         // 2) Предположить название ScreenParams и их пакет исходя из названия и пакета экрана.
         val screenParamsClassDeclaration: KSClassDeclaration = findScreenParamsFromConstructor(constructorParams)
-        // Если не смогли найти нужный экран, то идем по варианту 2.
+            // Если не смогли найти нужный экран, то идем по варианту 2.
             ?: generateScreenParamsFromScreenName(instance, resolver)
 
         // После определения параметров экрана нам необходимо определить параметры событий (intent), которыми
@@ -104,7 +104,7 @@ internal class FactoryGeneratorSymbolProcessor(
             .filter { param -> param.type.toTypeName() != screenIntentReceiveChannelType }
             .filter { param ->
                 (param.type.resolve().declaration as? KSClassDeclaration)
-                    ?.findParametrizedSuperTypeOrNull(SCREEN_PARAMS_CLASS) ==null
+                    ?.findParametrizedSuperTypeOrNull(SCREEN_PARAMS_CLASS) == null
             }
 
         // Блок кода который будет помещен в тело функции crate
@@ -190,7 +190,7 @@ internal class FactoryGeneratorSymbolProcessor(
         if (classDeclaration == null) {
             logger.error(
                 message = "ScreenParams not found and automatically resolved as ${name.canonicalName}, " +
-                        "but screenParams with current type not exist",
+                    "but screenParams with current type not exist",
                 symbol = instance,
             )
             error("${name.canonicalName} not exist")

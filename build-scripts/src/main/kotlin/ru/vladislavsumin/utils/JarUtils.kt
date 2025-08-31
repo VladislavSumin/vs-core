2a2afbd0225a4649df3a61dcc5f70b2966f26855
Vladislav Sumin <vladislavsumin@mail.ru>
2025-08-31 18:55:00 +0300

    add utils for build fatJar

---
+++ build-scripts/src/main/kotlin/ru/vladislavsumin/utils/JarUtils.kt
@@ -0,0 +1,59 @@
+package ru.vladislavsumin.utils
+
+import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.internal.extensions.stdlib.capitalized
+import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.register
+import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
+
+/**
+ * Creates tasks buildFarJar[flavor] and runJvm[flavor] for current [KotlinJvmTarget].
+ * Jar file will be stored at `build/libs/[jarName].jar`.
+ *
+ * @param mainClass main class for store in manifest inside jar and for running with jvmRun[flavor].
+ * @param flavor flavor name (default is main).
+ * @param jarName name for jar archive.
+ * @param duplicatesStrategy strategy for copy spec, sometime we may change it for some reason.
+ */
+fun KotlinJvmTarget.fatJar(
+    mainClass: String,
+    flavor: String = "main",
+    jarName: String = "${project.name}-fat",
+    duplicatesStrategy: DuplicatesStrategy = DuplicatesStrategy.FAIL,
+) {
+    compilations {
+        val main = getByName(flavor)
+        // See https://stackoverflow.com/questions/57168853/create-fat-jar-from-kotlin-multiplatform-project
+        project.tasks.register<Jar>("buildFatJar${flavor.capitalized()}") {
+            group = "application"
+
+            manifest {
+                attributes["Main-Class"] = mainClass
+            }
+
+            archiveBaseName.set(jarName)
+
+            from(main.output.classesDirs)
+
+            this.duplicatesStrategy = duplicatesStrategy
+
+            // doFirst is workaround
+            // from can`t accept provider as argument, see https://github.com/gradle/gradle/issues/22637
+            // we need to calculate dependencies before tusk run (not at configuration time)
+            doFirst {
+                val dependencies = main.runtimeDependencyFiles.map {
+                    check(it.exists()) { "${it.absolutePath} not exists" }
+                    if (it.isDirectory) it else project.zipTree(it)
+                }
+                from(dependencies)
+            }
+
+            exclude("META-INF/LICENSE")
+            exclude("META-INF/DEPENDENCIES")
+            exclude("META-INF/NOTICE")
+            exclude("META-INF/versions/**")
+            exclude("META-INF/*.kotlin_module")
+        }
+    }
+}

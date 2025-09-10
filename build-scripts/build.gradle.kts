90cd85cdfb8589697d8a6a6db0c375af06ef0da4
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-10 21:19:21 +0300

    make vsCoreLibs hack dependency form impl to api (#73)

--- build-scripts/build.gradle.kts
+++ build-scripts/build.gradle.kts
@@ -7,7 +7,7 @@ group = "ru.vladislavsumin"
 dependencies {
     // Мы хотим получать доступ к libs из наших convention плагинов, но гредл на текущий момент не умеет прокидывать
     // version catalogs. Поэтому используем костыль отсюда - https://github.com/gradle/gradle/issues/15383
-    implementation(files(vsCoreLibs.javaClass.superclass.protectionDomain.codeSource.location))
+    api(files(vsCoreLibs.javaClass.superclass.protectionDomain.codeSource.location))
 
     implementation(vsCoreLibs.gradlePlugins.kotlin.core)
     implementation(vsCoreLibs.gradlePlugins.kotlin.compose.compiler)

9ab9d31612e217aa72a788876a7c13fa66a95243
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-17 23:16:23 +0300

    Allow missing api project for impl-to-api-dependency.gradle.kts (#76)

--- build-scripts/src/main/kotlin/ru/vladislavsumin/convention/impl-to-api-dependency.gradle.kts
+++ build-scripts/src/main/kotlin/ru/vladislavsumin/convention/impl-to-api-dependency.gradle.kts
@@ -15,11 +15,14 @@ protectFromDslAccessors {
         sourceSets {
             commonMain.dependencies {
                 val suffix = "impl"
-                check(project.name.endsWith(suffix)) { "impl-to-api-dependency can apply only to impl modules" }
-                val apiProjectName = project.name.dropLast(suffix.length) + "api"
-                val apiProject = project.parent!!.findProject(apiProjectName)
-                checkNotNull(apiProject) { "api project $apiProjectName not found for project ${project.name}" }
-                api(apiProject)
+                if (project.name.endsWith(suffix)) {
+                    val apiProjectName = project.name.dropLast(suffix.length) + "api"
+                    val apiProject = project.parent!!.findProject(apiProjectName)
+                    apiProject?.let { api(it) }
+                } else {
+                    // Не подключаем ничего если не нашли подходящий проект, но и не падаем, так как это общий
+                    // конвеншен который может использоваться в иерархии с более чем одной имплементацией.
+                }
             }
         }
     }

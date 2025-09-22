5e730022b0fec423036f9a6c5477a572e8d96f83
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-22 21:31:57 +0300

    make fatJar return task provider

--- build-scripts/src/main/kotlin/ru/vladislavsumin/utils/JarUtils.kt
+++ build-scripts/src/main/kotlin/ru/vladislavsumin/utils/JarUtils.kt
@@ -1,6 +1,7 @@
 package ru.vladislavsumin.utils
 
 import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.internal.extensions.stdlib.capitalized
 import org.gradle.kotlin.dsl.invoke
@@ -21,33 +22,31 @@ fun KotlinJvmTarget.fatJar(
     flavor: String = "main",
     jarName: String = "${project.name}-fat",
     duplicatesStrategy: DuplicatesStrategy = DuplicatesStrategy.FAIL,
-) {
-    compilations {
-        val main = getByName(flavor)
-        // See https://stackoverflow.com/questions/57168853/create-fat-jar-from-kotlin-multiplatform-project
-        project.tasks.register<Jar>("buildFatJar${flavor.capitalized()}") {
-            group = "application"
+): TaskProvider<Jar> {
+    val main = compilations.getByName(flavor)
+    // See https://stackoverflow.com/questions/57168853/create-fat-jar-from-kotlin-multiplatform-project
+    return project.tasks.register<Jar>("buildFatJar${flavor.capitalized()}") {
+        group = "application"
 
-            manifest {
-                attributes["Main-Class"] = mainClass
-            }
-
-            archiveBaseName.set(jarName)
-
-            from(main.output.classesDirs)
-
-            this.duplicatesStrategy = duplicatesStrategy
-
-            val dependencies = main.runtimeDependencyFiles.map {
-                if (it.isDirectory) it else project.zipTree(it)
-            }
-            from(dependencies)
-
-            exclude("META-INF/LICENSE")
-            exclude("META-INF/DEPENDENCIES")
-            exclude("META-INF/NOTICE")
-            exclude("META-INF/versions/**")
-            exclude("META-INF/*.kotlin_module")
+        manifest {
+            attributes["Main-Class"] = mainClass
         }
+
+        archiveBaseName.set(jarName)
+
+        from(main.output.classesDirs)
+
+        this.duplicatesStrategy = duplicatesStrategy
+
+        val dependencies = main.runtimeDependencyFiles.map {
+            if (it.isDirectory) it else project.zipTree(it)
+        }
+        from(dependencies)
+
+        exclude("META-INF/LICENSE")
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/NOTICE")
+        exclude("META-INF/versions/**")
+        exclude("META-INF/*.kotlin_module")
     }
 }

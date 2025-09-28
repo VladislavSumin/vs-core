d478650b22dcfdf1f062d0b8b9b270c34a2eec09
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-28 23:34:23 +0300

    Add second approximation for list asyncTransform

--- core/coroutines/collection-utils/src/commonMain/kotlin/ru/vladislavsumin/core/coroutines/collectionUtils/ListExt.kt
+++ core/coroutines/collection-utils/src/commonMain/kotlin/ru/vladislavsumin/core/coroutines/collectionUtils/ListExt.kt
@@ -6,16 +6,17 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.joinAll
 import kotlin.math.min
+import kotlin.reflect.KClass
 
-public suspend fun <T, R> List<T>.asyncTransform(
+public suspend inline fun <T : Any, reified R : Any> List<T>.asyncTransform(
     bucketSize: Int = 32, // TODO вычислять динамически
     workerCount: Int = 4, // TODO смотреть на limitedParallelism
-    block: suspend (T) -> R?,
+    noinline block: suspend (T) -> R?,
 ): List<R> {
     return AsyncListTransformer(this, bucketSize, workerCount, block).transform()
 }
 
-private class AsyncListTransformer<T, R>(
+public class AsyncListTransformer<T : Any, R : Any>(
     private val original: List<T>,
     private val bucketSize: Int,
     private val workerCount: Int,
@@ -25,12 +26,11 @@ private class AsyncListTransformer<T, R>(
     // в создавшем его классе.
     private val firstUnprocessedBucket = atomic(0)
 
-    suspend fun transform(): List<R> = coroutineScope {
+    public suspend fun transform(): List<R> = coroutineScope {
 
         // Мы явно указали результату максимально возможный размер, что бы избежать изменения размера листа во время
         // выполнения. Таким образом мы можем спокойно писать в этот лист асинхронно.
-        val result = ArrayList<R?>(original.size)
-        repeat(original.size) { result.add(null) }
+        val result = arrayOfNulls<Any>(original.size)
 
         val lastBucket = original.size / bucketSize
 
@@ -65,11 +65,7 @@ private class AsyncListTransformer<T, R>(
                 result[resultIndex++] = result[index]
             }
         }
-        repeat(result.size - resultIndex) {
-            result.removeLast()
-        }
 
-        result.trimToSize()
-        result as List<R>
+        result.slice(0 until resultIndex) as List<R>
     }
 }

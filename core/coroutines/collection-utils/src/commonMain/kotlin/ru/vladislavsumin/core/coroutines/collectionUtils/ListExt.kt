c38c863a7b227e3feb4dca580d1e91d03d3fca7a
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-29 00:23:23 +0300

    rename asyncTransform to asyncMapNotNull

--- core/coroutines/collection-utils/src/commonMain/kotlin/ru/vladislavsumin/core/coroutines/collectionUtils/ListExt.kt
+++ core/coroutines/collection-utils/src/commonMain/kotlin/ru/vladislavsumin/core/coroutines/collectionUtils/ListExt.kt
@@ -4,22 +4,20 @@ import kotlinx.atomicfu.atomic
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.joinAll
 import kotlin.math.min
-import kotlin.reflect.KClass
 
-public suspend inline fun <T : Any, reified R : Any> List<T>.asyncTransform(
-    bucketSize: Int = 32, // TODO вычислять динамически
-    workerCount: Int = 4, // TODO смотреть на limitedParallelism
-    noinline block: suspend (T) -> R?,
-): List<R> {
-    return AsyncListTransformer(this, bucketSize, workerCount, block).transform()
-}
+public suspend fun <T : Any, R : Any> List<T>.asyncMapNotNull(
+    workerCount: Int = 4, // Мы не можем узнать current limited parallelism, тут нам нужна подсказка.
+    bucketSize: Int = min(32, size / workerCount + 1),
+    block: suspend (T) -> R?,
+): List<R> = AsyncListTransformer(original = this, workerCount, bucketSize, block).transform()
 
 public class AsyncListTransformer<T : Any, R : Any>(
     private val original: List<T>,
-    private val bucketSize: Int,
     private val workerCount: Int,
+    private val bucketSize: Int,
     private val block: suspend (T) -> R?,
 ) {
     // Вся байда с классом нужна из-за atomic. Он не умеет создаваться в функции и может использоваться только
@@ -27,9 +25,6 @@ public class AsyncListTransformer<T : Any, R : Any>(
     private val firstUnprocessedBucket = atomic(0)
 
     public suspend fun transform(): List<R> = coroutineScope {
-
-        // Мы явно указали результату максимально возможный размер, что бы избежать изменения размера листа во время
-        // выполнения. Таким образом мы можем спокойно писать в этот лист асинхронно.
         val result = arrayOfNulls<Any>(original.size)
 
         val lastBucket = original.size / bucketSize
@@ -52,6 +47,7 @@ public class AsyncListTransformer<T : Any, R : Any>(
                     originalIndex++
                 }
                 buckedEndIndexes[currentBucket] = resultIndex
+                if (!coroutineContext.isActive) break
             }
         }
 
--- core/coroutines/collection-utils/src/jvmTest/kotlin/ru/vladislavsumin/core/coroutines/collectionUtils/ListAsyncMapNotNullTest.kt
+++ core/coroutines/collection-utils/src/jvmTest/kotlin/ru/vladislavsumin/core/coroutines/collectionUtils/ListAsyncMapNotNullTest.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.withContext
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class ListAsyncTransformTest {
+class ListAsyncMapNotNullTest {
     @Test
     fun mapCollectionWithoutNulls() = runBlocking {
         val original = buildList<Int>(ELEMENT_COUNT) {
@@ -15,7 +15,7 @@ class ListAsyncTransformTest {
             }
         }
         withContext(Dispatchers.Default) {
-            val transformed = original.asyncTransform { -it }
+            val transformed = original.asyncMapNotNull { -it }
             val expected = original.map { -it }
             assertEquals(expected, transformed)
 
@@ -30,7 +30,7 @@ class ListAsyncTransformTest {
             }
         }
         withContext(Dispatchers.Default) {
-            val transformed = original.asyncTransform { if (it % 2 == 0) it else null }
+            val transformed = original.asyncMapNotNull { if (it % 2 == 0) it else null }
             val expected = original.mapNotNull { if (it % 2 == 0) it else null }
             assertEquals(expected, transformed)
 
@@ -38,7 +38,7 @@ class ListAsyncTransformTest {
     }
 
 
-    companion object {
+    companion object Companion {
         private const val ELEMENT_COUNT: Int = 1_000_000
     }
 }

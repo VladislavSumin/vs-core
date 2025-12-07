dcea96992b035e4a259af710978140f1e678fe6b
Vladislav Sumin <vladislavsumin@mail.ru>
2025-12-07 16:57:28 +0300

    fix missed publish record

--- core/fs/impl/build.gradle.kts
+++ core/fs/impl/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("ru.vladislavsumin.convention.kmp.all")
     // TODO FS not ready for publish now
-    id("ru.vladislavsumin.convention.preset.publish")
+    // id("ru.vladislavsumin.convention.preset.publish")
 }
 
 kotlin {

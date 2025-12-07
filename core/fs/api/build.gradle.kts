406d167763c25ce7580127e30987620995ad8c05
Vladislav Sumin <vladislavsumin@mail.ru>
2025-12-07 17:18:16 +0300

    fix fs version and group

--- core/fs/api/build.gradle.kts
+++ core/fs/api/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("ru.vladislavsumin.convention.kmp.all")
     // TODO FS not ready for publish now
     // id("ru.vladislavsumin.convention.preset.publish")
+    id("ru.vladislavsumin.convention.publication.group")
+    id("ru.vladislavsumin.convention.publication.version")
 }
 
 kotlin {

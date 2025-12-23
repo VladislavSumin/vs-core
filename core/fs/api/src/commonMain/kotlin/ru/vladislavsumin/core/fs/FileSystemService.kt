d1aecf3a74b5c0f2515936a67bef56c230c49baa
Vladislav Sumin <vladislavsumin@mail.ru>
2025-12-23 23:08:47 +0300

    add db support && fix missed "." issue

--- core/fs/api/src/commonMain/kotlin/ru/vladislavsumin/core/fs/FileSystemService.kt
+++ core/fs/api/src/commonMain/kotlin/ru/vladislavsumin/core/fs/FileSystemService.kt
@@ -7,4 +7,9 @@ public interface FileSystemService {
      * Возвращает путь к папке, предназначенной для хранения префов.
      */
     public fun getPreferencesDir(): Path
+
+    /**
+     * Возвращает путь к папке, предназначенной для хранения баз данных.
+     */
+    public fun getDatabaseDir(): Path
 }

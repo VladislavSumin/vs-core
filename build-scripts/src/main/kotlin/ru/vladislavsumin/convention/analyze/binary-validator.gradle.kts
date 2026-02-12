8892901a65f01c29084091b44cce92722f5286f6
Vladislav Sumin <vladislavsumin@mail.ru>
2026-02-12 18:17:57 +0300

    Remove @PublishedApi from public api

--- build-scripts/src/main/kotlin/ru/vladislavsumin/convention/analyze/binary-validator.gradle.kts
+++ build-scripts/src/main/kotlin/ru/vladislavsumin/convention/analyze/binary-validator.gradle.kts
@@ -7,3 +7,10 @@ package ru.vladislavsumin.convention.analyze
 plugins {
     id("org.jetbrains.kotlinx.binary-compatibility-validator")
 }
+
+apiValidation {
+    // Не учитываем @PublishedApi как публичное api.
+    // Вообще с этим могут быть нюансы бинарной совместимости, но сейчас я рассчитываю, что все модули собираются
+    // с одинаковой версией библиотеки.
+    nonPublicMarkers.add("kotlin.PublishedApi")
+}

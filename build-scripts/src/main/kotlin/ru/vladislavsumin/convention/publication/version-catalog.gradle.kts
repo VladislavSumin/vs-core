4dc4bec4d25fbf6b9ba0fb864c04fc35e686d037
Vladislav Sumin <vladislavsumin@mail.ru>
2026-02-23 22:27:21 +0300

    Fix version catalog publication

--- build-scripts/src/main/kotlin/ru/vladislavsumin/convention/publication/version-catalog.gradle.kts
+++ build-scripts/src/main/kotlin/ru/vladislavsumin/convention/publication/version-catalog.gradle.kts
@@ -1,6 +1,6 @@
 package ru.vladislavsumin.convention.publication
 
-import com.android.build.gradle.internal.tasks.factory.dependsOn
+import com.vanniktech.maven.publish.VersionCatalog
 import org.gradle.kotlin.dsl.`version-catalog`
 import ru.vladislavsumin.configuration.projectConfiguration
 
@@ -22,17 +22,6 @@ catalog {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("versionCatalog") {
-            this.artifactId = "vs-core-libs"
-            from(components["versionCatalog"])
-        }
-    }
-}
-
-// publishMavenPublicationToMavenCentralRepository not exists before evaluate.
-afterEvaluate {
-    val signTask = tasks.named("signVersionCatalogPublication")
-    tasks.named("publishMavenPublicationToMavenCentralRepository").dependsOn(signTask)
+mavenPublishing {
+    configure(VersionCatalog())
 }

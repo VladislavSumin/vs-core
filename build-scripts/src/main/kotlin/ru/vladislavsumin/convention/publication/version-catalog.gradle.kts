34a6598d96cfda161ed948683d3702c280c124e0
Vladislav Sumin <vladislavsumin@mail.ru>
2026-05-30 13:53:36 +0300

    Fix build.gradle.kts to use -P flag (#124)
    * Fix build.gradle.kts to use -P flag
    
    * fix detekt

--- build-scripts/src/main/kotlin/ru/vladislavsumin/convention/publication/version-catalog.gradle.kts
+++ build-scripts/src/main/kotlin/ru/vladislavsumin/convention/publication/version-catalog.gradle.kts
@@ -1,7 +1,6 @@
 package ru.vladislavsumin.convention.publication
 
 import com.vanniktech.maven.publish.VersionCatalog
-import gradle.kotlin.dsl.accessors._682b3ecf0fec931c9497ddf99a38843e.mavenPublishing
 import org.gradle.kotlin.dsl.`version-catalog`
 import ru.vladislavsumin.configuration.projectConfiguration
 

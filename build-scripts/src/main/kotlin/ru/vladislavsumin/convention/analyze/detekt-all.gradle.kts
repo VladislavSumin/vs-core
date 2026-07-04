f93a98f6f4766bc90f59c239a6bec09a3b7870e8
Vladislav Sumin <vladislavsumin@mail.ru>
2026-07-04 18:01:07 +0300

    Detekt empedded rule (#135)
    * make detekt config embedded
    
    * add custom plugin check base package
    
    * Revert "add custom plugin check base package"
    
    This reverts commit 854da49ea903b9b9f411da880c782fb1a235f592.

--- build-scripts/src/main/kotlin/ru/vladislavsumin/convention/analyze/detekt-all.gradle.kts
+++ build-scripts/src/main/kotlin/ru/vladislavsumin/convention/analyze/detekt-all.gradle.kts
@@ -1,10 +1,14 @@
 package ru.vladislavsumin.convention.analyze
 
+import ru.vladislavsumin.utils.registerDetektBaseConfigTask
+
 /**
  * Настройка detekt плагина по умолчанию для всех модулей.
  */
 
 check(project === rootProject) { "This convention may be applied only to root project" }
+
+registerDetektBaseConfigTask()
 
 allprojects {
     apply {
--- build-scripts/src/main/kotlin/ru/vladislavsumin/utils/DetektUtils.kt
+++ build-scripts/src/main/kotlin/ru/vladislavsumin/utils/DetektUtils.kt
@@ -1,10 +1,13 @@
 package ru.vladislavsumin.utils
 
 import dev.detekt.gradle.Detekt
+import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.register
 import java.io.File
+
+private object DetektConfigLoader
 
 /**
  * Регистрирует в данном [Project] задачу которая будет запускать detekt для "внешнего" модуля,
@@ -22,5 +25,37 @@ fun Project.registerExternalModuleDetektTask(
 }.also {
     if (dependsOnDetekt) {
         tasks.named("detekt").configure { dependsOn(it) }
+    }
+}
+
+/**
+ * Регистрирует таску [resolveDetektBaseConfig] в корневом проекте, которая копирует
+ * базовый конфиг detekt из classpath (упакован в JAR build-scripts) в build-директорию.
+ * Таска будет up-to-date благодаря [outputs.file] и [inputs.property].
+ */
+fun Project.registerDetektBaseConfigTask(): TaskProvider<DefaultTask> {
+    val resourceUrl = requireNotNull(
+        DetektConfigLoader::class.java.classLoader.getResource("config/analyze/detekt.yml"),
+    ) {
+        "detekt.yml not found in build-scripts classpath. " +
+            "Ensure it exists at build-scripts/src/main/resources/config/analyze/detekt.yml"
+    }
+
+    return tasks.register<DefaultTask>("resolveDetektBaseConfig") {
+        val outputFile = layout.buildDirectory.file("tmp/detekt-base-config.yml")
+        outputs.file(outputFile)
+        outputs.cacheIf { true }
+
+        inputs.property("configContentHash") {
+            resourceUrl.openStream().use { it.readBytes().contentHashCode() }
+        }
+
+        doLast {
+            val dest = outputFile.get().asFile
+            dest.parentFile.mkdirs()
+            resourceUrl.openStream().use { input ->
+                dest.outputStream().use { input.copyTo(it) }
+            }
+        }
     }
 }

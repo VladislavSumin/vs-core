adc6bb9721bf4ab370bb9705d7dc554cd1d71bfe
Vladislav Sumin <vladislavsumin@mail.ru>
2026-07-13 21:46:09 +0300

    Add stdout setup for default logger setup (#147)

--- core/logger/platform/src/iosMain/kotlin/ru/vladislavsumin/core/logger/platform/PlatformLoggerFactory.ios.kt
+++ core/logger/platform/src/iosMain/kotlin/ru/vladislavsumin/core/logger/platform/PlatformLoggerFactory.ios.kt
@@ -3,6 +3,7 @@ package ru.vladislavsumin.core.logger.platform
 @Suppress("UNUSED_PARAMETER")
 internal actual fun createPlatformLoggerFactory(
     logPath: LogPath,
+    stdout: Boolean,
 ): ru.vladislavsumin.core.logger.manager.ExternalLoggerFactory {
     TODO("Not yet implemented")
 }
